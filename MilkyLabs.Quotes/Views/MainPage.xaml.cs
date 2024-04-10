c5144c3d1076165b590b091102f8dd0fb4a2f969
ltachiUchiha <itachiuchihadev@gmail.com>
2024-04-10 23:09:15 +0300

    Added project template

---
+++ MilkyLabs.Quotes/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using MilkyLabs.Quotes.ViewModels;
+
+namespace MilkyLabs.Quotes.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
