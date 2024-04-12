ff602c7c19f58da00fc77abbc2005613b70c70c9
Katy248 <petrovanton247@gmail.com>
2024-04-12 16:19:05 +0300

    Update quote

--- MilkyLabs.Quotes.Core/Models/Quote.cs
+++ MilkyLabs.Quotes.Core/Models/Quote.cs
@@ -6,5 +6,6 @@ public record class Quote(string Text, DateTime LastUpdated, DateTime Creation, 
 {
     Quote SetText(string text) => this with { Text = text, LastUpdated = DateTime.Now }
     Quote SetAuthor(string author) => this with { Author = author, LastUpdated = DateTime.Now };
+    Quote SetSource(string source) => this with { Source = source, LastUpdated = DateTime.Now };
     public static Quote Create(string Text) => new Quote(Text, DateTime.Now, DateTime.Now);
 }
